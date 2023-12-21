0d4e23a291ffd40a186a22a2d0c6703c6879ea1d
DustinLWagner <DustinWagner@gmail.com>
2023-12-21 13:29:57 -0600

    first commit

---
+++ starterP2/assignments.js
@@ -0,0 +1,181 @@
+'use strict';
+/*
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} and it's capital city is ${capitalCity}.`
+
+}
+const unitedStates = describeCountry('United States', '332 Million people', 'Washington');
+const angola = describeCountry('Angola', '32 Million people', 'Luanda');
+const eritrea = describeCountry('Eritrea', '3.5 Million people', 'Asmara');
+
+
+console.log(unitedStates);
+console.log(angola);
+console.log(eritrea);
+
+// Function Declaration
+const worldPop = (7900);
+const usaPop = (332);
+const angoPop = (32);
+const eritPop = (3.5);
+const chinaPop = (1441);
+
+
+function percentageOfWorld1(population) {
+    return (population / worldPop) * 100;
+}
+const chinaPercent = percentageOfWorld1(chinaPop);
+const usPercent = percentageOfWorld1(usaPop);
+const angoPercent = percentageOfWorld1(angoPop);
+console.log(angoPercent, chinaPercent, usPercent);
+
+
+//Function Expression
+const percentageOfWorld2 = function (population) {
+    return (population / worldPop) * 100;
+}
+const perPop2 = percentageOfWorld2(usaPop);
+const perPop3 = percentageOfWorld2(chinaPop);
+const perPop4 = percentageOfWorld2(angoPop);
+
+console.log(perPop2, perPop3, perPop4);
+
+// Arrow Function
+const percentageOfWorld3 = population => (population / worldPop) * 100;
+const percUsPop = (percentageOfWorld3(usaPop));
+const percAngoPop = (percentageOfWorld3(angoPop));
+const percEritPop = (percentageOfWorld3(eritPop));
+console.log(percAngoPop, percEritPop, percUsPop);
+
+// Functions calling other Functions //
+
+function describePopulation(country, population) {
+    const percentage = percentageOfWorld1(population);
+    const description = `${country} has ${population} million people, which is about ${percentage}% of the world.`;
+    return console.log(description);
+}
+describePopulation('United States', `${usaPop}`);
+describePopulation('Angola', `${angoPop}`);
+describePopulation('China', `${chinaPop}`);
+describePopulation('Butthole', 7900);
+
+
+
+////////////////
+/////////////   CHALLENGE #1
+///////////////
+
+
+// create calculate function
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+//creating values with calcAvg function to be used in checkWinner
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+
+//create checkWinner function
+function checkWinner(avgDolphins, avgKoalas) {
+    {
+        if (avgDolphins >= 2 * avgKoalas) {
+            console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+
+        } else if (avgKoalas >= 2 * avgDolphins) {
+
+            console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+
+        } else
+            console.log(`No team wins`);
+    }
+}
+//calling the function using values from calcAvg score func
+checkWinner(scoreDolphins, scoreKoalas);
+
+//Updating the scores
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+//calling function with updated scores
+checkWinner(scoreDolphins, scoreKoalas);
+
+///////////////////////////////////////////////////////////
+
+
+
+//ARRAAYYSSS INTRO //////////
+
+// 1. Create an array containing 4 population values of 4 countries of your choice.
+// You may use the values you have been using previously. Store this array into a
+// variable called 'populations'
+// 2. Log to the console whether the array has 4 elements or not (true or false)
+// 3. Create an array called 'percentages' containing the percentages of the
+// world population for these 4 population values. Use the function
+// 'percentageOfWorld1' that you created earlier to compute the 4
+// percentage values
+
+const worldPop = (7900);
+const usaPop = (332);
+const angoPop = (32);
+const eritPop = (3.5);
+const chinaPop = (1441);
+
+function percentageOfWorld1(population) {
+    return (population / worldPop) * 100;
+}
+
+const populations = [usaPop, angoPop, eritPop, chinaPop];
+const popLength = populations.length;
+console.log(popLength === 4);
+
+const percentages = [(percentageOfWorld1(usaPop)), percentageOfWorld1(angoPop), percentageOfWorld1(eritPop), percentageOfWorld1(chinaPop)];
+console.log(percentages);
+console.log(usaPop, 'and ', percentages[0]);
+
+
+
+///////////////////////////////////////////////////////////
+
+
+
+// 1. Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+
+const neighbors = ['Canada', 'Mexico', 'Water'];
+
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+
+neighbors.push('Utopia');
+console.log(neighbors);
+
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+
+neighbors.pop(); //Removes last element
+
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+
+if (neighbors.includes('Germany')) {
+    console.log('Probably a central European country :D');
+} else (
+    console.log('Probably not a central European country :D')
+);
+
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+console.log(neighbors.indexOf('Water'));
+neighbors[2] = 'Ocean';
+//let index = neighbors.indexOf('Water');
+// if (index !== -1) {
+//     neighbors['Water'] = 'Ocean';
+// }
+console.log(neighbors);
+
+
+ */
+
+
+
+
---
+++ starterP2/script.js
@@ -0,0 +1,224 @@
+//  Strict Mode //
+'use strict';
+// and thats it, activates for entire sciprt. Must be first.
+// strict mode forbids certain things and creates visual errors in certain situations
+/*
+
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// const interface = 'Audio'; Reserved word throws error
+// const private = 5345;
+
+
+function logger() {
+    console.log('My Name is Dustin');
+}
+
+// calling / running / invoking Function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+
+// Function declaration (Can call a Function Declaration before you define it, even though its not a good idea but can be done if necessary)
+
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+
+// Funtion expression (can NOT Call Function Expression before it is defined)
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+*/
+
+
+// Arrow Function
+// const calc3 = birthYear => 2037 - birthYear;
+// const age3 = calc3(1991);
+// console.log(age3);
+
+
+
+/*
+const yearsUntilRetire = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retire = 65 - age;
+    // return retire;
+    return `${firstName} retires in ${retire} years.`;
+}
+
+console.log(yearsUntilRetire(1991, 'Dustin'));
+console.log(yearsUntilRetire(1980, 'Bob'));
+
+
+function cutPieces(fruit) {
+    return fruit * 3;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetire = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retire = 65 - age;
+
+    if (retire > 0) {
+        console.log(`${firstName} retires in ${retire} years. `);
+        return retire;
+    } else {
+        console.log(`${firstName} has already retired! `)
+        return -1;
+
+    }
+}
+console.log(yearsUntilRetire(1991, "Dustin"));
+console.log(yearsUntilRetire(1980, "Mike"));
+
+
+/////////  ARRAYS /////////
+
+const friends = ['Michael', 'Steven', 'Peter']; //Literal Syntax
+
+console.log(friends);
+
+const y = new Array(1999, 1984, 2008, 2020);
+
+console.log(friends[0]); //Michael
+console.log(friends[2]);//Peter
+
+console.log(friends.length); //get how many items in array
+console.log(friends[friends.length - 1]); // get last in array
+friends[2] = 'Jay';
+console.log(friends);
+
+
+const firstName = 'Dustin';
+const dustin = [firstName, 'Wagner', 2024 - 1986, 'Coder', friends];
+
+console.log(dustin);
+
+// exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+
+//////// Basic Array Operations (Methods) //////////////
+
+//Add Elements
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLength = friends.push('Jay');  //Adds to end of Array
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John'); // Adds to begining of Array
+console.log(friends);
+
+// Remove Elements
+friends.pop();
+const popped = friends.pop(); // last
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend named Steven');
+}
+
+
+///////////////////////////////////////////////////////////////
+//      CODING CHALLENGE #2
+//////////////////////////////////////////////////////////////
+
+
+// function calcTip(bill) {
+//     if (bill >= 50 && bill <= 300) {
+//         let tip = (bill * .15);
+//         return tip;
+
+//     } else {
+//         let tip = (bill * .2);
+//         return tip;
+//     }
+// }
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+// calcTip(100);
+
+const bills = [125, 555, 44];
+
+const tips = [(calcTip(bills[0])), (calcTip(bills[1])), (calcTip(bills[2]))];
+
+const totals = [((bills[0]) + (tips[0])), ((bills[1]) + (tips[1])), ((bills[2]) + (tips[2]))];
+
+console.log(bills);
+
+console.log(tips);
+
+console.log(totals);
+
+
+*/
+
