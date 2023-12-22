b3003fe5257511cb73796cab6387551f7183ea61
DustinLWagner <DustinWagner@gmail.com>
2023-12-22 15:32:29 -0600

    created styles.css not working yet, created user2

--- charList/app.js
+++ charList/app.js
@@ -4,9 +4,9 @@
 
 //object 1
 const user1 = {
-    name: 'Edwin', //string
+    name: 'Edwin',
     class: 'Rogue',
-    race: 'gnome',       // number
+    race: 'Gnome',
     level: 15,
     isHalfling: true, // boolean
     skills: ['Acrobatics', 'Cantrips', 'Deception'], // array
@@ -21,7 +21,7 @@ const user1 = {
     },
 
     sayDesc: function () {
-        console.log(`  Hello, I am ${this.name} and I am a ${this.race} year old level ${this.level} ${this.class}. `);
+        console.log(`  Hello, I am ${this.name} and I am a  level ${this.level} ${this.class} ${this.race}.`);
 
         if (this.isHalfling === true) {
             const yesHalf = `I am rather large for my kind and I excel at ${this.skills[0]}, ${this.skills[1]}, and ${this.skills[2]}. `;
@@ -33,27 +33,20 @@ const user1 = {
     }
 
 };
-
-user1.sayName(); // USER here is assigning THIS above
-//access the different properties inside the object
-// console.log(user.skills);
-// console.log(user.age, user.name);
-user1.sayClass();
-user1.sayDesc();
 
 
 //object 2
 const user2 = {
-    name: 'Renflower', //string
-    class: 'Rogue',
-    race: 'Elf',       // number
-    level: 15,
-    isHalfling: true, // boolean
-    skills: ['Acrobatics', 'Cantrips', 'Deception'], // array
-    // functions attatched to an Object are Methods
+    name: 'Renflower',
+    class: 'Druid',
+    race: 'Elf',
+    level: 12,
+    isHalfling: false, // boolean
+    skills: ['Druid Magic', 'Wild Shape', 'Survival Skills'], // array
+
 
     sayName: function () {
-        console.log(this.name); // THIS here is assigned when invoking the function, USER in this case, so THIS is assigned from USER
+        console.log(this.name);
     },
 
     sayClass: function () {
@@ -61,7 +54,7 @@ const user2 = {
     },
 
     sayDesc: function () {
-        console.log(`  Hello, I am ${this.name} and I am a ${this.race} year old level ${this.level} ${this.class}. `);
+        console.log(`  Hello, I am ${this.name} and I am a  level ${this.level} ${this.class} ${this.race}.`);
 
         if (this.isHalfling === true) {
             const yesHalf = `I am rather large for my kind and I excel at ${this.skills[0]}, ${this.skills[1]}, and ${this.skills[2]}. `;
@@ -74,3 +67,13 @@ const user2 = {
 
 };
 
+user1.sayName();
+// user1.sayClass();
+// USER here is assigning THIS above
+//access the different properties inside the object
+// console.log(user.skills);
+// console.log(user1.level, user1.race);
+
+user1.sayDesc();
+user2.sayName();
+user2.sayDesc();
