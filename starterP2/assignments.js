01c78e5c526684c160841b018c9b50f07c98d991
DustinLWagner <DustinWagner@gmail.com>
2023-12-23 18:45:19 -0600

    finished 2:43 assignment

--- starterP2/assignments.js
+++ starterP2/assignments.js
@@ -216,26 +216,33 @@ console.log(neighbors);
 
 // 1. Add a method called 'describe' to the 'myCountry' object. This method will log a string to the console, similar to the string logged in the previous assignment, but this time using the 'this' keyword.
 
-// 2. Call the 'describe' method
+// 2. Call the 'describe' method  
 
-// 3. Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'.'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.
+// 3. Add a method called 'checkIsland' to the 'myCountry' object. This method will set a new property on the object, called 'isIsland'.'isIsland' will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.  
 
 const myCountry = {
-    country: 'USA',
+    country: 'The USA',
     capital: 'Washington, D.C.',
     language: 'English',
     population: 334.9,
     neighbours: ['Canada', 'Mexico'],
 
-
-
-    describe: function () {
-        return ` ${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
-    },
-
+    // checkIsland: function () {
+    //     return this.neighbours ? true : false;
+    // },
     checkIsland: function () {
-        return this.neighbours.length <= 0
+        return this.checkIsland = !Boolean(this.neighbours.length);
+    },
+    // describe: function () {
+    //     return ` ${this.country} has ${this.population} million ${this.language}-speaking people,${this.neighbours.length > 0 ? ` ${this.neighbours.length}` + ' neighbouring countries,' : ``} and it's capital is ${this.capital} `
+    // },
+    describe2: function () {
+        console.log(` ${this.country} has ${this.population} million ${this.language}-speaking people,${this.neighbours.length > 0 ? ` ${this.neighbours.length}` + ' neighbouring countries,' : ``} and it's capital is ${this.capital} `)
     },
 };
-console.log(myCountry.describe());
+
+// console.log(myCountry.describe());
 console.log(myCountry.checkIsland());
+myCountry.describe2();
+
+
